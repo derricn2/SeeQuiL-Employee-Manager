0f68f1058424785b55c0f404ce016162d93f9af2
derricn2 <derricn2@gmail.com>
2023-07-13 18:33:41 -0700

    added function to view all employees

--- server.js
+++ server.js
@@ -80,4 +80,27 @@ async function viewAllRoles() {
     promptMainMenu();
 };
 
+// function to view employees
+async function viewAllEmployees() {
+    try {
+      const query = `
+        SELECT employee.id,
+               employee.first_name,
+               employee.last_name,
+               role.title,
+               department.name AS department, role.salary,
+        CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+        FROM employee
+        INNER JOIN role ON employee.role_id = role.id
+        INNER JOIN department ON role.department_id = department.id
+        LEFT JOIN employee AS manager ON employee.manager_id = manager.id
+      `;
+      const [employees] = await db.query(query);
+      console.table(employees);
+    } catch (error) {
+      console.error('Error executing query:', error);
+    }
+    promptMainMenu();
+  };
+
 promptMainMenu();
